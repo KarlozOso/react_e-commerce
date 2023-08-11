2b8ef55fc64c87526d40f15464943d967eab69a6
KarlozOso <karlozoso90@gmail.com>
2023-08-11 02:19:40 -0600

    Project E-commerce

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+
+
+const Navbar = () => {
+  return(
+    
+    <div className="navbar">
+    <div className="wrapper">
+      <div className="left"></div>
+      <div className="center"></div>
+      <div className="right"></div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar;
