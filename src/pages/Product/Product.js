324c7503ae9244dd62aa41e10b44fd2f619648b7
KarlozOso <karlozoso90@gmail.com>
2023-08-12 16:59:00 -0600

    advance afternoom

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Product.scss";
 
 const Product = () => {
-  return <div>Product</div>
+  const [selectedImg, setSelectedImg] = useState(0);
+
+  const images = [
+    'https://images.pexels.com/photos/356170/pexels-photo-356170.jpeg?auto=compress&cs=tinysrtb&w=1600',
+    'https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrtb&w=1600'
+  ];
+
+  return (
+    <div className='product'>
+      <div className='left'>
+        <div className='images'>
+          <img src={images[0]} alt="Product 1" onClick={() => setSelectedImg(0)} />
+          <img src={images[1]} alt='Product 2' onClick={() => setSelectedImg(1)} />
+        </div>
+        <div className='mainImg'>
+          <img src={images[selectedImg]} alt={`Product ${selectedImg + 1}`} />
+        </div>
+      </div>
+      <div className='right'></div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
